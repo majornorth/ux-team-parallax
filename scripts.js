0ee85eb27a595497744b9cdc435f4ca830706c9d
Stewart McCoy <mccoy.stewart@gmail.com>
2015-12-12 17:11:59 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// (function getScrollOffsets() {
+//     var x = window.pageXOffset;
+//     var y = window.pageYOffset;
+
+//     console.log(x + " + " + y);
+// }());
+
+
+function dw_getScrollOffsets() {
+    var doc = document, w = window;
+    var x, y, docEl;
+
+    if ( typeof w.pageYOffset === 'number' ) {
+        x = w.pageXOffset;
+        y = w.pageYOffset;
+    } else {
+        docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat')?
+                doc.documentElement: doc.body;
+        x = docEl.scrollLeft;
+        y = docEl.scrollTop;
+    }
+    return {x:x, y:y};
+}
+
+window.onscroll = function() {
+    var off = dw_getScrollOffsets();
+    console.log(off.y);
+
+    if (off.y >= '1001' && off.y <= '1999') {
+        var one = document.getElementsByClassName('one')[0];
+        one.classList.add('fadeout');
+        var two = document.getElementsByClassName('two')[0];
+        two.classList.remove('hide');
+    }
+
+    if (off.y <= '1000') {
+        var one = document.getElementsByClassName('one')[0];
+        one.classList.remove('hide');
+        var two = document.getElementsByClassName('two')[0];
+        two.classList.add('hide');
+    }
+}
